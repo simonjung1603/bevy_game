0e32636caf26b02b990765840cb60cc356c59cba
Simon Jung <simonjung1603@gmail.com>
2025-04-01 16:36:42 +0200

    Fix deployment hanging with build.rs use postbuild script instead

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,20 +1,12 @@
-use std::env::{current_dir, current_exe};
-
-use axum::{routing::get, Router};
+use axum::Router;
 use tower_http::services::{ServeDir, ServeFile};
-async fn hello_world() -> &'static str {
-    "Hello world! Go to /game to see your Bevy build."
-}
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let game_dir = current_exe()?.parent().unwrap().join("game");
-    let asset_dir = current_dir()?.join("game/assets");
-    println!("{}", game_dir.display());
-    println!("{}", asset_dir.display());
-    let router = Router::new()//.route("/", get(hello_world))
-                                      .fallback_service(ServeDir::new(&game_dir)
-                                      .not_found_service(ServeFile::new(&game_dir.join("index.html"))))
-                                      .nest_service("/assets", ServeDir::new(&asset_dir));
+    let router = Router::new()
+        .fallback_service(
+            ServeDir::new("game/dist").not_found_service(ServeFile::new("game/dist/index.html")),
+        )
+        .nest_service("/assets", ServeDir::new("game/assets"));
     Ok(router.into())
 }
